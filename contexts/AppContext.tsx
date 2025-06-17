7469b41c95cbc2f356da0c890fc82a62c6a0d5fd
farajallah <57751651+farajallah@users.noreply.github.com>
2025-06-17 12:11:55 +0300

    Updated AppContext.tsx

--- contexts/AppContext.tsx
+++ contexts/AppContext.tsx
@@ -14,7 +14,7 @@ const DEFAULT_SETTINGS: AppSettings = {
   notificationsEnabled: true,
   soundEnabled: true,
   timeFormat: '12h',
-  apiUrl: 'https://accidental-glamorous-aphid.glitch.me/medicines.json',
+  apiUrl: 'https://mediminder.tiiny.site/medicines.json',
   defaultTimes: {
     once: ['08:00'],
     twice: ['08:00', '20:00'],
